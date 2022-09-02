ebbec9e7b236806ead215e36093f1f8061705344
Tamikon <angeldontag@rambler.ru>
2022-09-02 11:43:38 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = true;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.Location = new System.Drawing.Point(12, 12);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 18;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(838, 667);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 10D;
+            this.map.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.map_OnMarkerClick);
+            this.map.Load += new System.EventHandler(this.map_Load);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(875, 54);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Билетный киоск";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(875, 80);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(220, 20);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Aвтомат по продаже билетов";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(220)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.labelInfo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(875, 118);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 561);
+            this.panel1.TabIndex = 3;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInfo.Location = new System.Drawing.Point(14, 31);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(267, 530);
+            this.labelInfo.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(88, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Информация";
+            // 
+            // close
+            // 
+            this.close.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.close.Location = new System.Drawing.Point(1166, -6);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(41, 42);
+            this.close.TabIndex = 4;
+            this.close.Text = "x";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(871, 31);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(187, 20);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Выберите тип точки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1194, 691);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.map);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public class Bilet
+        {
+            public ClassFeatures[] features { get; set; }
+        }
+        public class ClassProperties
+        {
+            public ClassAttributes Attributes { get; set; }
+        }
+        public class ClassAttributes
+        {
+            public string AdmArea { get; set; }
+            public string District { get; set; }
+            public string Location { get; set; }
+            public string ActualLocaton { get; set; }
+            public string OnTerritoryOfMoscow { get; set; }
+            public string WorkingHours { get; set; }
+            public string PointType { get; set; }
+            public string BoothNumber { get; set; }
+            public string PaymentType { get; set; }
+
+        }
+        public class ClassСoordinates
+        {
+            public float[] coordinates { get; set; }
+        }
+        public class ClassFeatures
+        {
+            public ClassСoordinates geometry { get; set; }
+            public ClassProperties properties { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+        GMapOverlay ListOfMarkers1 = new GMapOverlay("markers");
+        GMapOverlay ListOfMarkers2 = new GMapOverlay("markers");
+        public void map_Load(object sender, EventArgs e)
+        {
+            // Перетаскивание карты левой кнопкой мыши
+            map.DragButton = MouseButtons.Left;
+            // Чья карта используется
+            map.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            // Загрузка этой точки на карте
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            //стартовая точка
+            map.Position = new GMap.NET.PointLatLng(55.751694, 37.617218);
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string url = "https://apidata.mos.ru/v1/datasets/673/features?api_key=111c24c258eb074aae14175f1c004270";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamreader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamreader.ReadToEnd();
+            }
+            Bilet bilet = JsonConvert.DeserializeObject<Bilet>(response);
+            map.Overlays.Add(ListOfMarkers1);
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < bilet.features.Length; i++)
+                {
+                    if (bilet.features[i].properties.Attributes.PointType == "билетный киоск")
+                    {
+                        GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(bilet.features[i].geometry.coordinates[1], bilet.features[i].geometry.coordinates[0]), new Bitmap(@"Icon/i1.png"));
+                        ListOfMarkers1.Markers.Add(marker);
+                    }
+                }
+                map.Overlays.Add(ListOfMarkers1);
+            }
+            else ListOfMarkers1.Clear();
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            string url = "https://apidata.mos.ru/v1/datasets/673/features?api_key=111c24c258eb074aae14175f1c004270";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamreader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamreader.ReadToEnd();
+            }
+            Bilet bilet = JsonConvert.DeserializeObject<Bilet>(response);
+            map.Overlays.Add(ListOfMarkers2);
+            if (checkBox2.Checked)
+            {
+                for (int i = 0; i < bilet.features.Length; i++)
+                {
+                    if (bilet.features[i].properties.Attributes.PointType == "автомат по продаже билетов")
+                    {
+                        GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(bilet.features[i].geometry.coordinates[1], bilet.features[i].geometry.coordinates[0]), new Bitmap(@"Icon/i2.png"));
+                        ListOfMarkers2.Markers.Add(marker);
+                    }
+                }
+                map.Overlays.Add(ListOfMarkers2);
+            }
+            else ListOfMarkers2.Clear();
+        }
+        private void map_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            string url = "https://apidata.mos.ru/v1/datasets/673/features?api_key=111c24c258eb074aae14175f1c004270";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+            using (StreamReader streamreader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamreader.ReadToEnd();
+            }
+            Bilet bilet = JsonConvert.DeserializeObject<Bilet>(response);
+
+            for (int i = 0; i < bilet.features.Length; i++)
+            {
+                if ((item.Position.Lat == bilet.features[i].geometry.coordinates[1])&& (item.Position.Lng == bilet.features[i].geometry.coordinates[0]))
+                {
+
+                    labelInfo.Text = "КООРДИНАТЫ:\n" + Convert.ToString(bilet.features[i].geometry.coordinates[1]) + "; " + Convert.ToString(bilet.features[i].geometry.coordinates[0])+"\n\n"+"АДМИНИСТРАТИВНЫЙ ОКРУГ:\n"+ Convert.ToString(bilet.features[i].properties.Attributes.AdmArea) + "\n\n" + "РАЙОН:\n" + Convert.ToString(bilet.features[i].properties.Attributes.District) + "\n\n" + "АДРЕСНЫЙ ОРИЕНТИР:\n" + Convert.ToString(bilet.features[i].properties.Attributes.Location) + "\n\n" + "ФАКТИЧЕСКРЕ МЕСТОПОЛОЖЕНИЕ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.ActualLocaton) + "\n\n" + "НА ТЕРРИТОРИИ МОСКВЫ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.OnTerritoryOfMoscow) + "\n\n" + "ГРАФИК РАБОТЫ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.WorkingHours) + "\n\n" + "ТИП ТОЧКИ ПРОДАЖИ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.PointType) + "\n\n" + "НОМЕР ТОЧКИ ПРОДАЖИ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.BoothNumber) + "\n\n" + "ВИД ОПЛАТЫ:\n" + Convert.ToString(bilet.features[i].properties.Attributes.PaymentType);
+
+                }
+            }
+        }
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //перемещение окна
+        Point lastPoint;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+    }
+}
